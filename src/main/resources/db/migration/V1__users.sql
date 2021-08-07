2b11673cf2a750b27907d0844e159e4935b69048
Prashant <prashant@Prashants-MacBook-Pro.local>
2021-08-07 09:14:05 -0700

    Added create queries for few tables

--- src/main/resources/db/migration/V1__users.sql
+++ src/main/resources/db/migration/V1__users.sql
@@ -3,6 +3,42 @@ CREATE TABLE IF NOT EXISTS `users` (
   first_name varchar(50),
   last_name varchar(50),
   user_uuid varchar(50),
-  created timestamp DEFAULT CURRENT_TIMESTAMP ,
-  updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP ,
+  updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
+
+CREATE TABLE IF NOT EXISTS `user-preferences` (
+  id int primary key AUTO_INCREMENT,
+  user_id varchar(50),
+  setting_name varchar(20),
+  setting_values blob
+)
+
+CREATE TABLE IF NOT EXISTS `user-preferences` (
+  id int primary key AUTO_INCREMENT,
+  user_id varchar(50),
+  setting_name varchar(20),
+  setting_values blob
+)
+
+CREATE TABLE IF NOT EXISTS `user-preferences` (
+  id int primary key AUTO_INCREMENT,
+  user_id varchar(50),
+  url varchar(20),
+  updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS `user-preferences` (
+  id int primary key AUTO_INCREMENT,
+  user_id varchar(50),
+  url varchar(20),
+  updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS `user-preferences` (
+  id int primary key AUTO_INCREMENT,
+  user_id varchar(50),
+  url varchar(255),
+  updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
