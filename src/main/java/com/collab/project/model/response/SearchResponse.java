6f1c9b9b2f14ab30818341c25dde0e73032ff69d
Prashant <prashant@Prashants-MacBook-Pro.local>
2021-08-15 14:25:00 +0530

    Using sets to remove the duplicate in the list after query

--- src/main/java/com/collab/project/model/response/SearchResponse.java
+++ src/main/java/com/collab/project/model/response/SearchResponse.java
@@ -5,16 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class SearchResponse {
+    @NotNull
     String entityType;
 
     String name;
 
     String slug;
 
+    @NotNull
     Object id;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SearchResponse that = (SearchResponse) o;
+        return entityType.equals(that.entityType) && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(entityType, id);
+    }
 }
--- src/main/java/com/collab/project/service/impl/SearchServiceImpl.java
+++ src/main/java/com/collab/project/service/impl/SearchServiceImpl.java
@@ -11,8 +11,7 @@ import io.jsonwebtoken.lang.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -25,35 +24,38 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public List<SearchResponse> getSearchResults(String queryStr) {
-        List<SearchResponse> searchResponseList = new ArrayList<>();
+        Set<SearchResponse> artistResponseSet = new HashSet<>();
+        Set<SearchResponse> artResponseSet = new HashSet<>();
         String queryStrSlug = Strings.replace(queryStr, " ", "-");
 
         List<ArtCategory> artCategories = artCategoryRepository.findBySlug(queryStrSlug);
-        for (ArtCategory artCategory : artCategories) searchResponseList.add(new SearchResponse(Enums.EntityType.ART.toString(),
+        for (ArtCategory artCategory : artCategories) artResponseSet.add(new SearchResponse(Enums.EntityType.ART.toString(),
                 artCategory.getArtName(),
                 artCategory.getSlug(),
                 artCategory.getId()));
 
         artCategories = artCategoryRepository.findBySlugStartsWith(queryStrSlug);
-        for (ArtCategory artCategory : artCategories) searchResponseList.add(new SearchResponse(Enums.EntityType.ART.toString(),
+        for (ArtCategory artCategory : artCategories) artResponseSet.add(new SearchResponse(Enums.EntityType.ART.toString(),
                 artCategory.getArtName(),
                 artCategory.getSlug(),
                 artCategory.getId()));
 
-
         List<Artist> artists = artistRepository.findBySlug(queryStrSlug);
-        for (Artist artist : artists) searchResponseList.add(new SearchResponse(Enums.EntityType.ARTIST.toString(),
+        for (Artist artist : artists) artistResponseSet.add(new SearchResponse(Enums.EntityType.ARTIST.toString(),
                 artist.getFirstName() + " " + artist.getLastName(),
                 artist.getSlug(),
                 artist.getArtistId()));
 
 
         artists = artistRepository.findBySlugStartsWith(queryStrSlug);
-        for (Artist artist : artists) searchResponseList.add(new SearchResponse(Enums.EntityType.ARTIST.toString(),
+        for (Artist artist : artists) artistResponseSet.add(new SearchResponse(Enums.EntityType.ARTIST.toString(),
                 artist.getFirstName() + " " + artist.getLastName(),
                 artist.getSlug(),
                 artist.getArtistId()));
 
-        return searchResponseList;
+        List<SearchResponse> resultSet = new ArrayList<>();
+        resultSet.addAll(artResponseSet);
+        resultSet.addAll(artistResponseSet);
+        return resultSet;
     }
 }
