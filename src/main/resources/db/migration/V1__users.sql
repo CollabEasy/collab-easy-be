f1411f9435c8887a12f8efc7cde1293270248e09
Prashant <prashant@Prashants-MacBook-Pro.local>
2021-08-21 22:39:12 +0530

    Updated SQL queries after conflicts

--- src/main/resources/db/migration/V1__users.sql
+++ src/main/resources/db/migration/V1__users.sql
@@ -6,12 +6,8 @@ CREATE TABLE IF NOT EXISTS artists (
   last_name varchar(50),
   email varchar(100) not null unique,
   phone_number varchar(15) unique,
-<<<<<<< HEAD
-  county varchar(20),
-=======
   country varchar(20),
   gender varchar(10),
->>>>>>> apis
   profile_pic_url varchar(255),
   timezone varchar(5),
   bio varchar(512),
@@ -22,27 +18,15 @@ CREATE TABLE IF NOT EXISTS artists (
   key(id)
 )'
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS `artist-preferences` (
-=======
 CREATE INDEX IF NOT EXISTS `slug_index` ON `artists` (slug);
 
 CREATE TABLE IF NOT EXISTS `artist_preferences` (
->>>>>>> apis
   id int AUTO_INCREMENT,
   artist_id varchar(50),
   setting_name varchar(20),
   setting_values blob,
   key(id),
-<<<<<<< HEAD
-<<<<<<< HEAD
-  primary key(artist_id)
-=======
   unique key(artist_id, setting_name),
->>>>>>> apis
-=======
-  primary key(artist_id),
->>>>>>> 4441c94f61d4c250496431c44cdad2f0cdade1b1
   foreign key(artist_id) references artists(artist_id)
 )
 
@@ -52,23 +36,10 @@ CREATE TABLE IF NOT EXISTS `artist-samples` (
   url varchar(20),
   updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   key(id),
-  primary key(artist_id),
+  primary key(artist_id)
   foreign key(artist_id) references artists(artist_id)
 )
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS `art-categories` (
-  id int AUTO_INCREMENT,
-  name varchar(50),
-  description varchar(255),
-  approved boolean,
-  key(id),
-  primary key(name)
-)
-
---not yet added.
-CREATE TABLE IF NOT EXISTS `artist-categories` (
-=======
 CREATE TABLE IF NOT EXISTS `art_categories` (
   id int AUTO_INCREMENT,
   art_name varchar(50),
@@ -79,23 +50,15 @@ CREATE TABLE IF NOT EXISTS `art_categories` (
 )
 
 CREATE TABLE IF NOT EXISTS `artist_categories` (
->>>>>>> apis
   id int AUTO_INCREMENT,
   artist_id varchar(50),
   art_id int,
   key(id),
   foreign key(artist_id) references artists(artist_id),
-<<<<<<< HEAD
-  foreign key(art_id) references art-categories(id)
-)
-
-CREATE TABLE IF NOT EXISTS `collab-requests` (
-=======
   foreign key(art_id) references art_categories(id)
 )
 
 CREATE TABLE IF NOT EXISTS `collab_requests` (
->>>>>>> apis
   id int AUTO_INCREMENT,
   request_id varchar(50),
   sender_id varchar(50),
@@ -107,19 +70,12 @@ CREATE TABLE IF NOT EXISTS `collab_requests` (
   updated_at timestamp,
   key(id),
   primary key(request_id),
-<<<<<<< HEAD
-  foreign key(sender_id) references artists(artist_id)
-)
-
-CREATE TABLE IF NOT EXISTS `collab-reviews` (
-=======
   key(sender_id),
   key(receiver_id),
   foreign key(sender_id) references artists(artist_id)
 )
 
 CREATE TABLE IF NOT EXISTS `collab_reviews` (
->>>>>>> apis
     id int AUTO_INCREMENT,
     request_id varchar(50),
     artist_id varchar(50),
@@ -139,15 +95,11 @@ CREATE TABLE IF NOT EXISTS `notifications` (
     notification_data blob,
     notif_read bool,
     notif_view_type varchar(25),
-    created_at timestamp,
+    created_at timestamp
     key(id),
     primary key(notif_id),
-<<<<<<< HEAD
-=======
     key(artist_id),
->>>>>>> apis
     foreign key(artist_id) references artists(artist_id)
  )
-
 
 
