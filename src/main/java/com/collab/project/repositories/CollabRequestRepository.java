920dbe0c0d63d9ffcb7817b5e0ee91c44d8ce4f1
Tanish Gupta <tanishg@zeta.tech>
2021-08-29 15:31:31 +0530

    Added collab

--- src/main/java/com/collab/project/repositories/CollabRequestRepository.java
+++ src/main/java/com/collab/project/repositories/CollabRequestRepository.java
@@ -1,11 +1,8 @@
 package com.collab.project.repositories;
 
 import com.collab.project.model.collab.CollabRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
@@ -14,15 +11,8 @@ import java.util.Optional;
 
 @Repository
 public interface CollabRequestRepository extends PagingAndSortingRepository<CollabRequest, Long>, JpaSpecificationExecutor<CollabRequest> {
-
-//  @Query("SELECT c from CollabRequest c WHERE c.sender_id=:senderId and receiver_id=:receiverId and status in :status")
-//  public List<CollabRequest> findBySenderAndReceiver(@Param("senderId") String senderId, @Param("receiverId")  String receiverId, @Param("status") List<String> status);
-//
-//  @Query("SELECT c from CollabRequest c WHERE c.sender_id=:senderId and receiver_id=:receiverId and status in :status")
-//  public List<CollabRequest> findByReceiverAndSender(@Param("receiverId")  String receiverId, @Param("senderId") String senderId, @Param("status") List<String> status);
-
+  
   public List<CollabRequest> findBySenderIdAndReceiverIdAndStatusIn(String senderId, String receiverId, List<String> status);
-  //public CollabRequest findById(String senderId, String receiverId, List<String> status);
 
 
 }
