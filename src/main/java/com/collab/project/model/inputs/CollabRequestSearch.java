fe638fdfce57e56ba1a6ec56a463ec143fff1a9e
Ankit Bathla <ankitbathla@chegg.com>
2021-09-26 20:29:18 +0530

    updated collab model prop

--- src/main/java/com/collab/project/model/inputs/CollabRequestSearch.java
+++ src/main/java/com/collab/project/model/inputs/CollabRequestSearch.java
@@ -1,13 +1,20 @@
 package com.collab.project.model.inputs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CollabRequestSearch {
-
-    private final Long collabRequestId;
-    private final String status;
-    private final String senderId;
-    private final String receiverId;
-
+    @JsonProperty("collabRequestId")
+    private Long collabRequestId;
+    @JsonProperty("status")
+    private String status;
+    @JsonProperty("senderId")
+    private String senderId;
+    @JsonProperty("receiverId")
+    private String receiverId;
 }
