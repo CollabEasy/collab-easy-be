8f7e1d31f2e27b894654ad3939e4a7f4bef1726c
Ankit Bathla <ankitbathla@chegg.com>
2021-10-10 22:37:32 +0530

    Added implementation fr get artist details

--- src/main/java/com/collab/project/controller/LoginController.java
+++ src/main/java/com/collab/project/controller/LoginController.java
@@ -56,8 +56,8 @@ public class LoginController {
 
     @RequestMapping(value = "/details", method = RequestMethod.GET)
     public ResponseEntity<?> update() {
-        String artistId = authUtils.getArtistId();
-        return ResponseEntity.ok(artistId);
+        Artist artist = artistService.getArtistById(authUtils.getArtistId());
+        return ResponseEntity.ok(artist);
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
