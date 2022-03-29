97cdae4962aef3018bff5facfdd8710eab4dc050
rahul gupta <rabbal1892@rabbal1892.local>
2022-03-28 19:08:16 -0500

    update prospectus api

--- src/main/java/com/collab/project/controller/ArtistSocialProspectusController.java
+++ src/main/java/com/collab/project/controller/ArtistSocialProspectusController.java
@@ -1,8 +1,10 @@
 package com.collab.project.controller;
 
+import com.collab.project.model.artist.Artist;
 import com.collab.project.model.inputs.ArtistSocialProspectusInput;
 import com.collab.project.model.response.SuccessResponse;
 import com.collab.project.model.socialprospectus.ArtistSocialProspectus;
+import com.collab.project.service.ArtistService;
 import com.collab.project.service.ArtistSocialProspectusService;
 import com.collab.project.util.AuthUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ import static com.collab.project.helpers.Constants.FALLBACK_ID;
 public class ArtistSocialProspectusController {
 
     @Autowired
+    private ArtistService artistService;
+
+    @Autowired
     private ArtistSocialProspectusService artistSocialProspectusService;
 
     @PostMapping
@@ -31,8 +36,14 @@ public class ArtistSocialProspectusController {
 
     @GetMapping
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public ResponseEntity<SuccessResponse> getArtistSocialProspectus() {
-        List<ArtistSocialProspectus> prospectus = artistSocialProspectusService.getSocialProspectByArtistId(AuthUtils.getArtistId());
+    public ResponseEntity<SuccessResponse> getArtistSocialProspectus(@RequestParam(required = false) String handle) {
+        Artist artist;
+        if (handle == null || handle.equals("")) {
+            artist = artistService.getArtistById(AuthUtils.getArtistId());
+        } else {
+            artist = artistService.getArtistBySlug(handle);
+        }
+        List<ArtistSocialProspectus> prospectus = artistSocialProspectusService.getSocialProspectByArtistId(artist.getArtistId());
         return new ResponseEntity<>(new SuccessResponse(prospectus), HttpStatus.OK);
     }
 
