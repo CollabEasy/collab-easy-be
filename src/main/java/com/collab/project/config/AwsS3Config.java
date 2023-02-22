691e7599986ed22a338a357b02b8609877ff8b59
Prashant Joshi <prashant.joshi056@gmail.com>
2023-02-22 19:22:54 +0000

    Update AwsS3Config.java

--- src/main/java/com/collab/project/config/AwsS3Config.java
+++ src/main/java/com/collab/project/config/AwsS3Config.java
@@ -9,7 +9,7 @@ import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class AwsS3Config {
 
     // Region will be read from the application.properties file  during the application intialization.
