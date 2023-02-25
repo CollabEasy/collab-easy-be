7d3d899ce1e73d792b04f8ce50aa871676a55149
Prashant Joshi <prashantjoshi@192.168.1.100>
2023-02-25 14:10:26 +0000

    Updated sql queries for all the tables

--- src/main/java/com/collab/project/config/AwsS3Config.java
+++ src/main/java/com/collab/project/config/AwsS3Config.java
@@ -5,18 +5,18 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// @Configuration
+@Configuration
 public class AwsS3Config {
 
-    // Region will be read from the application.properties file  during the application intialization.
-    // @Value("${aws.s3.region}")
+     // Region will be read from the application.properties file  during the application initialization.
+    @Value("${aws.s3.region}")
     private String region = "us-east-1";
 
-//     @Bean
+    @Bean
     public AmazonS3 getAmazonS3Cient() {
         final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(System.getenv("ACCESS_KEY"),
                                                                                 System.getenv("SECRET_KEY"));
--- src/main/java/com/collab/project/service/impl/ArtistSampleServiceImpl.java
+++ src/main/java/com/collab/project/service/impl/ArtistSampleServiceImpl.java
@@ -27,10 +27,10 @@ import java.util.List;
 @Service
 public class ArtistSampleServiceImpl implements ArtistSampleService {
 
-    String bucketName = "wondor-samples";
+    String bucketName = "artist-samples";
 
-//     @Autowired
-//     S3Utils s3Utils;
+     @Autowired
+     S3Utils s3Utils;
 
     @Autowired
     ArtistSampleRepository artistSampleRepository;
@@ -41,101 +41,101 @@ public class ArtistSampleServiceImpl implements ArtistSampleService {
     @Async
     @Override
     public ArtInfo uploadFile(String artistId, String caption, String fileType, MultipartFile fileToUpload) throws IOException, NoSuchAlgorithmException {
-//         ArtSample artSample = null;
-//         String fileExtension = FilenameUtils.getExtension(fileToUpload.getOriginalFilename());
-//         switch (fileType) {
-//             case Constants.AUDIO:
-//                 if (!Constants.ALLOWED_AUDIO_FORMAT.contains(fileExtension)) {
-//                     throw new IllegalStateException("Invalid file extension");
-//                 }
-//                 artSample = uploadAudio(artistId, caption, fileToUpload, fileExtension);
-//                 break;
-//             case Constants.VIDEO:
-//                 if (!Constants.ALLOWED_VIDEO_FORMAT.contains(fileExtension)) {
-//                     throw new IllegalStateException("Invalid file extension");
-//                 }
-//                 artSample = uploadVideo(artistId, caption, fileToUpload, fileExtension);
-//                 break;
-//             case Constants.IMAGE:
-//                 if (!Constants.ALLOWED_IMAGE_FORMAT.contains(fileExtension)) {
-//                     throw new IllegalStateException("Invalid file extension");
-//                 }
-//                 artSample = uploadImage(artistId, caption, fileToUpload, fileExtension);
-//                 break;
-//             default:
-//                 throw new IllegalStateException("Invalid file type for upload");
-//         }
+         ArtSample artSample = null;
+         String fileExtension = FilenameUtils.getExtension(fileToUpload.getOriginalFilename());
+         switch (fileType) {
+             case Constants.AUDIO:
+                 if (!Constants.ALLOWED_AUDIO_FORMAT.contains(fileExtension)) {
+                     throw new IllegalStateException("Invalid file extension");
+                 }
+                 artSample = uploadAudio(artistId, caption, fileToUpload, fileExtension);
+                 break;
+             case Constants.VIDEO:
+                 if (!Constants.ALLOWED_VIDEO_FORMAT.contains(fileExtension)) {
+                     throw new IllegalStateException("Invalid file extension");
+                 }
+                 artSample = uploadVideo(artistId, caption, fileToUpload, fileExtension);
+                 break;
+             case Constants.IMAGE:
+                 if (!Constants.ALLOWED_IMAGE_FORMAT.contains(fileExtension)) {
+                     throw new IllegalStateException("Invalid file extension");
+                 }
+                 artSample = uploadImage(artistId, caption, fileToUpload, fileExtension);
+                 break;
+             default:
+                 throw new IllegalStateException("Invalid file type for upload");
+         }
 
-//         ArtInfo artInfo = new ArtInfo(
-//                 artSample.getCaption(),
-//                 artSample.getFileType(),
-//                 artSample.getOriginalUrl(),
-//                 artSample.getThumbnailUrl(),
-//                 artSample.getCreatedAt()
-//         );
+         ArtInfo artInfo = new ArtInfo(
+                 artSample.getCaption(),
+                 artSample.getFileType(),
+                 artSample.getOriginalUrl(),
+                 artSample.getThumbnailUrl(),
+                 artSample.getCreatedAt()
+         );
         return null;
     }
 
     private ArtSample uploadImage(String artistId, String caption, MultipartFile fileToUpload, String fileExtension) throws NoSuchAlgorithmException, IOException {
         // Creating original file
-//         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
-//         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
+         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
+         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
 
-//         // Creating thumbnail file
-//         FileUtils.createThumbnail(fileToUpload, fileName + "_thumb." + fileExtension);
-//         File thumbFile = new File(fileName + "_thumb." + fileExtension);
+         // Creating thumbnail file
+         FileUtils.createThumbnail(fileToUpload, fileName + "_thumb." + fileExtension);
+         File thumbFile = new File(fileName + "_thumb." + fileExtension);
 
-//         String originalURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
-//         String thumbnailURL = s3Utils.uploadFileToS3Bucket(bucketName, thumbFile, artistId + "/thumbnails", fileName + "." + fileExtension);
+         String originalURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
+         String thumbnailURL = s3Utils.uploadFileToS3Bucket(bucketName, thumbFile, artistId + "/thumbnails", fileName + "." + fileExtension);
 
-//         thumbFile.delete();
-//         file.delete();
-//         ArtSample artSample = new ArtSample(
-//                 Constants.FALLBACK_ID,
-//                 artistId, originalURL,
-//                 thumbnailURL, caption, Constants.IMAGE,
-//                 new Timestamp(System.currentTimeMillis()));
-//         artistSampleRepository.save(artSample);
+         thumbFile.delete();
+         file.delete();
+         ArtSample artSample = new ArtSample(
+                 Constants.FALLBACK_ID,
+                 artistId, originalURL,
+                 thumbnailURL, caption, Constants.IMAGE,
+                 new Timestamp(System.currentTimeMillis()));
+         artistSampleRepository.save(artSample);
         return null;
     }
 
     private ArtSample uploadAudio(String artistId, String caption, MultipartFile fileToUpload, String fileExtension) throws NoSuchAlgorithmException, IOException {
-//         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
-//         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
+         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
+         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
 
-//         String fileURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
-//         file.delete();
+         String fileURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
+         file.delete();
 
-//         ArtSample artSample = new ArtSample(
-//                 Constants.FALLBACK_ID,
-//                 artistId, fileURL,
-//                 fileURL, caption, Constants.AUDIO,
-//                 new Timestamp(System.currentTimeMillis()));
-//         artistSampleRepository.save(artSample);
+         ArtSample artSample = new ArtSample(
+                 Constants.FALLBACK_ID,
+                 artistId, fileURL,
+                 fileURL, caption, Constants.AUDIO,
+                 new Timestamp(System.currentTimeMillis()));
+         artistSampleRepository.save(artSample);
         return null;
     }
 
     private ArtSample uploadVideo(String artistId, String caption, MultipartFile fileToUpload, String fileExtension) throws NoSuchAlgorithmException, IOException {
-//         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
-//         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
-//         File thumbFile = new File(fileName + ".png");
-//         boolean thumbNailCreated = FileUtils.createThumbnailFromVideo(fileName + "." + fileExtension, fileName + ".png");
+         String fileName = Utils.getSHA256(artistId).substring(0, 15) + "_" + System.currentTimeMillis();
+         File file = FileUtils.convertMultiPartFileToFile(fileToUpload, fileName + "." + fileExtension);
+         File thumbFile = new File(fileName + ".png");
+         boolean thumbNailCreated = FileUtils.createThumbnailFromVideo(fileName + "." + fileExtension, fileName + ".png");
 
-//         String originalURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
-//         file.delete();
+         String originalURL = s3Utils.uploadFileToS3Bucket(bucketName, file, artistId + "/originals", fileName + "." + fileExtension);
+         file.delete();
 
-//         String thumbnailURL = "https://postimg.cc/18TvM41x";
-//         if (thumbNailCreated) {
-//             thumbnailURL = s3Utils.uploadFileToS3Bucket(bucketName, thumbFile, artistId + "/thumbnails", fileName + ".png");
-//             thumbFile.delete();
-//         }
+         String thumbnailURL = "https://postimg.cc/18TvM41x";
+         if (thumbNailCreated) {
+             thumbnailURL = s3Utils.uploadFileToS3Bucket(bucketName, thumbFile, artistId + "/thumbnails", fileName + ".png");
+             thumbFile.delete();
+         }
 
-//         ArtSample artSample = new ArtSample(
-//                 Constants.FALLBACK_ID,
-//                 artistId, originalURL,
-//                 thumbnailURL, caption, Constants.VIDEO,
-//                 new Timestamp(System.currentTimeMillis()));
-//         artistSampleRepository.save(artSample);
+         ArtSample artSample = new ArtSample(
+                 Constants.FALLBACK_ID,
+                 artistId, originalURL,
+                 thumbnailURL, caption, Constants.VIDEO,
+                 new Timestamp(System.currentTimeMillis()));
+         artistSampleRepository.save(artSample);
         return null;
     }
 
@@ -147,19 +147,19 @@ public class ArtistSampleServiceImpl implements ArtistSampleService {
 
     @Override
     public void deleteArtSample(String artistId, ArtInfo artInfo) {
-//         List<ArtSample> artSamples = artistSampleRepository.findByArtistId(artistId);
-//         for (ArtSample sample : artSamples) {
-//             if (sample.getOriginalUrl().equals(artInfo.getOriginalUrl())) {
-//                 artistSampleRepository.delete(sample);
-//                 break;
-//             }
-//         }
+         List<ArtSample> artSamples = artistSampleRepository.findByArtistId(artistId);
+         for (ArtSample sample : artSamples) {
+             if (sample.getOriginalUrl().equals(artInfo.getOriginalUrl())) {
+                 artistSampleRepository.delete(sample);
+                 break;
+             }
+         }
 
-//         String originalFilePath = getFilePath(artInfo.getOriginalUrl());
-//         String thumbnailFilePath = getFilePath(artInfo.getThumbnailUrl());
+         String originalFilePath = getFilePath(artInfo.getOriginalUrl());
+         String thumbnailFilePath = getFilePath(artInfo.getThumbnailUrl());
 
-//         s3Utils.removeFileFromS3Bucket(bucketName, originalFilePath);
-//         s3Utils.removeFileFromS3Bucket(bucketName, thumbnailFilePath);
+         s3Utils.removeFileFromS3Bucket(bucketName, originalFilePath);
+         s3Utils.removeFileFromS3Bucket(bucketName, thumbnailFilePath);
     }
 
     @Override
